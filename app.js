063ca7b9153fcaa2cd7ee74addea81005e26a209
ibbi <hi@ibbi.io>
2018-05-07 14:17:27 -0400

    lit search

--- app.js
+++ app.js
@@ -4,12 +4,7 @@
 // Initialize firebase
 
 const config = {
-  apiKey: "AIzaSyBHt-dtGoAs6akJbKeGdufatEUpnJ8QPa0",
-  authDomain: "my-project-1523381980704.firebaseapp.com",
-  databaseURL: "https://my-project-1523381980704.firebaseio.com",
-  projectId: "my-project-1523381980704",
-  storageBucket: "my-project-1523381980704.appspot.com",
-  messagingSenderId: "88935170519"
+
 };
 firebase.initializeApp(config);
 
